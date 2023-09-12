9e0ff08c93a5f6fd9335607e9d93051844c62ace
Maria <zaush@bk.ru>
2023-09-12 13:29:01 +0300

    changed default port of target

--- config/target/config.go
+++ config/target/config.go
@@ -11,7 +11,7 @@ type Config struct {
 	SleepDuration  time.Duration `env:"HANDLE_SLEEP_TIME" envDefault:"1s"`
 	TickerDuration time.Duration `env:"TICKER" envDefault:"10s"`
 	Strategy       string        `env:"STRATEGY"`
-	Port           int           `env:"PORT" envDefault:"8081"`
+	Port           int           `env:"PORT" envDefault:"8082"`
 }
 
 func LoadConfig() (Config, error) {
